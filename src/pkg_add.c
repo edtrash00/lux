95cdfdc376f749064339ba4fc166dd1ba1a98313
Katsuke00 <katsuke00@gmail.com>
2017-08-08 19:04:29 -0300

    add pkg_add

--- src/pkg_add.c
+++ src/pkg_add.c
@@ -1,9 +1,57 @@
 /* This file is part of the PkgUtils from EltaninOS
  * See LICENSE file for copyright and license details.
  */
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/* TODO */
+#include "db.h"
+#include "fs.h"
+#include "pkg.h"
+
+static int
+pkg_add(const char *path)
+{
+	int rval = 0;
+	Package *pkg;
+	struct node *current;
+
+	if (!(pkg = open_db(path)))
+		err(1, "open_db %s", path);
+
+	for (current = pkg->dirs; current; current = current->next)
+		rval |= copy_file(current->data, PKG_DIR);
+
+	for (current = pkg->files; current; current = current->next)
+		rval |= copy_file(current->data, PKG_DIR);
+
+	rval |= copy_file(path, PKG_DIR);
+
+	close_db(pkg);
+
+	return rval;
+}
+
+static int
+usage(void)
+{
+	fprintf(stderr, "usage: %s package ...\n", getprogname());
+	exit(1);
+}
+
 int
 main(int argc, char *argv[])
 {
+	int rval = 0;
+
+	setprogname(argv[0]);
+	argc--, argv++;
+
+	if (!argc)
+		usage();
+
+	for (; *argv; argv++)
+		rval |= pkg_add(*argv);
+
+	return rval;
 }
