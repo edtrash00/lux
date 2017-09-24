6d778b6778e58188bd902314ddd6210f5f272e0b
Katsuke00 <katsuke00@disroot.org>
2017-09-24 19:59:20 -0300

    add new macros to pkg header

--- inc/pkg.h
+++ inc/pkg.h
@@ -4,14 +4,18 @@
 /* PKG_DIR: ROOT  PATH
  * PKG_LDB: LOCAL DATABASE
  * PKG_RDB: REPO  DATABASE
- * PKG SRC: REPO  FILE
- * PKG TMP: TMP   DIR
+ * PKG_SRC: REPO  FILE
+ * PKG_TMP: TMP   DIR
+ * PKG_FMT: PKG   FORMAT
+ * PKG_SIG: PKG   SIGNATURE
  */
 #define PKG_DIR "/"
 #define PKG_LDB "/var/pkg/ldb"
 #define PKG_RDB "/var/pkg/rdb"
 #define PKG_SRC "/var/pkg/source.list"
 #define PKG_TMP "/tmp"
+#define PKG_FMT ".tar.gz"
+#define PKG_SIG ".sig"
 
 struct node {
 	void *data;
