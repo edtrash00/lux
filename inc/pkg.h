bb34a2b4eab6ffcf21a812db67e6cbf5c25deba6
Katsuke00 <katsuke00@disroot.org>
2017-09-26 22:54:25 -0300

    move PKG_* macros from pkg.h to lux.h

--- inc/pkg.h
+++ inc/pkg.h
@@ -1,19 +1,3 @@
-/* PKG_DIR: ROOT  PATH
- * PKG_LDB: LOCAL DATABASE
- * PKG_RDB: REPO  DATABASE
- * PKG_SRC: REPO  FILE
- * PKG_TMP: TMP   DIR
- * PKG_FMT: PKG   FORMAT
- * PKG_SIG: PKG   SIGNATURE
- */
-#define PKG_DIR "/"
-#define PKG_LDB "/var/pkg/ldb"
-#define PKG_RDB "/var/pkg/rdb"
-#define PKG_SRC "/var/pkg/source.list"
-#define PKG_TMP "/tmp"
-#define PKG_FMT ".tar.gz"
-#define PKG_SIG ".sig"
-
 struct node {
 	void *data;
 	struct node *next;
--- src/lux.h
+++ src/lux.h
@@ -2,6 +2,22 @@
 #include "compat.h"
 #include "pkg.h"
 
+/* PKG_DIR: ROOT  PATH
+ * PKG_LDB: LOCAL DATABASE
+ * PKG_RDB: REPO  DATABASE
+ * PKG_SRC: REPO  FILE
+ * PKG_TMP: TMP   DIR
+ * PKG_FMT: PKG   FORMAT
+ * PKG_SIG: PKG   SIGNATURE
+ */
+#define PKG_DIR "/"
+#define PKG_LDB "/var/pkg/ldb"
+#define PKG_RDB "/var/pkg/rdb"
+#define PKG_SRC "/var/pkg/source.list"
+#define PKG_TMP "/tmp"
+#define PKG_FMT ".tar.gz"
+#define PKG_SIG ".sig"
+
 int add(Package *pkg, const char *);
 int del(Package *pkg, const char *);
 int fetch(Package *pkg);
