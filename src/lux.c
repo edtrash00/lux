698d8d8b5c34f13ded55fb85782a57751c4373b0
Katsuke00 <katsuke00@disroot.org>
2017-09-30 12:06:02 -0300

    fix info message

--- src/lux.c
+++ src/lux.c
@@ -135,17 +135,19 @@ info(Package *pkg)
 	    "Description: %s\n",
 	    pkg->name, pkg->version, pkg->license, pkg->description);
 
-	puts("\nDependencies:");
-	for (np = pkg->rdeps; np; np = np->next)
+	if ((np = pkg->rdeps))
+		puts("\nDependencies:");
+	for (; np; np = np->next)
 		printf("R: %s\n", (char *)np->data);
 	for (np = pkg->mdeps; np; np = np->next)
 		printf("M: %s\n", (char *)np->data);
 
-	puts("\nFiles:");
-	for (np = pkg->dirs; np; np = np->next)
+	if ((np = pkg->dirs))
+		puts("\nFiles:");
+	for (; np; np = np->next)
 		printf("D: %s\n", (char *)np->data);
 	for (np = pkg->files; np; np = np->next)
-		printf("M: %s\n", (char *)np->data);
+		printf("F: %s\n", (char *)np->data);
 
 	return 0;
 }
