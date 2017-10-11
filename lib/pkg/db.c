d29745733d675944e7d9e44229f4aa46f5c9c0cb
Katsuke00 <katsuke00@disroot.org>
2017-10-11 15:55:49 -0300

    add comments to hashes

--- lib/pkg/db.c
+++ lib/pkg/db.c
@@ -6,19 +6,19 @@
 #include "pkg.h"
 
 /* hash values */
-#define Name        6382843298
-#define LongName    249786571779427685
-#define Version     229390708560831
-#define License     229372341555262
-#define Description 13751386334653244867U
-#define LongDesc    249786571779069171
-#define ByteSize    6383314208
-#define BytePkgSize 229388381659868
-#define RDependency 229394969610032
-#define MDependency 7569334749620571
-#define Directory   193404666
-#define File        6382564547
-#define Flag        6382568041
+#define Name        6382843298            /* name        */
+#define LongName    249786571779427685    /* long-name   */
+#define Version     229390708560831       /* version     */
+#define License     229372341555262       /* license     */
+#define Description 13751386334653244867U /* description */
+#define LongDesc    249786571779069171    /* long-desc   */
+#define ByteSize    6383314208            /* size        */
+#define BytePkgSize 229388381659868       /* pkgsize     */
+#define RDependency 229394969610032       /* run-dep     */
+#define MDependency 7569334749620571      /* make-dep    */
+#define Directory   193404666             /* dir         */
+#define File        6382564547            /* file        */
+#define Flag        6382568041            /* flag        */
 
 static unsigned long
 hash(char *str)
