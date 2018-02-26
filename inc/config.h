5dfdc1e1f92a3b6dec6f759573604e3c958f0d3b
Katsuke00 <katsuke00@disroot.org>
2018-02-26 02:52:09 -0300

    change default config

--- inc/config.h
+++ inc/config.h
@@ -1,11 +1,10 @@
 #define PKG_DIR "/"
-#define PKG_ETC "/etc/format/"
 #define PKG_LDB "/var/pkg/local/"
 #define PKG_RDB "/var/pkg/remote/"
 #define PKG_TMP "/var/pkg/cache/"
 
 #define PKG_SRC "url"
-#define PKG_FDB "__rdb"
+#define PKG_FDB "remote.db"
 #define PKG_FMT ".tzz"
 #define PKG_SIG ".sig"
 
