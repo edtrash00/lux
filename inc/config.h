bf09cfac44c6c53070e7f4403cebefcca68d7f28
Katsuke00 <katsuke00@disroot.org>
2018-03-02 12:00:31 -0300

    remove unecessary trailing slash

--- inc/config.h
+++ inc/config.h
@@ -1,7 +1,7 @@
 #define PKG_DIR "/"
-#define PKG_LDB "/var/pkg/local/"
-#define PKG_RDB "/var/pkg/remote/"
-#define PKG_TMP "/var/pkg/cache/"
+#define PKG_LDB "/var/pkg/local"
+#define PKG_RDB "/var/pkg/remote"
+#define PKG_TMP "/var/pkg/cache"
 
 #define PKG_SRC "url"
 #define PKG_FDB "remote.db"
